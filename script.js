8ed9a9c9d9c0ace8585bd870ffc63d792cd7ef6e
Nicholas Christopher <curiousnicklee@gmail.com>
2020-10-16 14:14:24 -0700

    Added default values for prompts

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function generate() {
-	let firstName = prompt("Enter your First Name");
-	let lastName = prompt("Enter your Last Name");
-	let studentID = prompt("Enter your Student ID");
+	let firstName = prompt("Enter your First Name", "John");
+	let lastName = prompt("Enter your Last Name", "Doe");
+	let studentID = prompt("Enter your Student ID", "123456");
 	document.getElementById("username").innerHTML = "<strong>USERNAME: </strong>" + firstName.charAt(0) + lastName + studentID.substr(2);
 	document.getElementById("email").innerHTML = "<strong>EMAIL: </strong>" + firstName.charAt(0) + lastName + studentID.substr(2) + "@student.pps.net";
 	document.getElementById("password").innerHTML = "<strong>PASSWORD: </strong>Pps" + studentID;
