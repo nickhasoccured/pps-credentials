845ae63ec90579f04cd189f7b3dd7d14a5b63caf
Nicholas Christopher <curiousnicklee@gmail.com>
2020-12-18 22:41:32 -0800

    Redesign
    - Uses form elements instead of prompts
    - Added copy buttons
    - Support for <6th Grade passwords
    - Switched to MIT License

--- script.js
+++ script.js
@@ -1,8 +1,43 @@
-function generate() {
-	let firstName = prompt("Enter your First Name", "John");
-	let lastName = prompt("Enter your Last Name", "Doe");
-	let studentID = prompt("Enter your Student ID", "123456");
-	document.getElementById("username").innerHTML = "<strong>USERNAME: </strong>" + firstName.charAt(0) + lastName + studentID.substr(2);
-	document.getElementById("email").innerHTML = "<strong>EMAIL: </strong>" + firstName.charAt(0) + lastName + studentID.substr(2) + "@student.pps.net";
-	document.getElementById("password").innerHTML = "<strong>PASSWORD: </strong>Pps" + studentID;
+function refreshDOBEnabled() {
+	const dateOfBirth = document.getElementById("dateOfBirth");
+
+	document.getElementById("higherGrade").checked
+		? (dateOfBirth.disabled = true)
+		: (dateOfBirth.disabled = false);
+}
+
+function displayResults() {
+	const dateOfBirth = document.getElementById("dateOfBirth").value.split("-");
+	const documentPassword = document.getElementById("password");
+	document.getElementById("higherGrade").checked
+		? (documentPassword.value = `Pps${document
+				.getElementById("studentID")
+				.value.substr(0, 6)}`)
+		: (documentPassword.value = `${dateOfBirth[1]}${dateOfBirth[2]}`);
+
+	const username = `${document.getElementById("firstName").value.charAt(0)}${
+		document.getElementById("lastName").value
+	}`;
+
+	document.getElementById("email").value = `${username}@student.pps.net`;
+	document.getElementById("username").value = username;
+}
+
+function copyElementValue(elementID) {
+	const element = document.getElementById(elementID);
+
+	element.select();
+	element.setSelectionRange(0, 99999);
+
+	document.execCommand("copy");
+
+	alert(`Copied ${elementID}`);
+}
+
+function displayPassword() {
+	const password = document.getElementById("password");
+
+	document.getElementById("showPassword").checked
+		? (password.type = "text")
+		: (password.type = "password");
 }
